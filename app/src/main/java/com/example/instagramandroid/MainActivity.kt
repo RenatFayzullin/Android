2998f1385466abcfa8c01841f69b0c52a7638bc0
RenatFayzullin <61165967+RenatFayzullin@users.noreply.github.com>
2020-09-21 15:48:32 +0300

    instagram clone

--- app/src/main/java/com/example/instagramandroid/MainActivity.kt
+++ app/src/main/java/com/example/instagramandroid/MainActivity.kt
@@ -9,6 +9,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var flag = true
+
+        edit.setOnClickListener {
+            if(flag) {
+                edit.text = "Применить изменения"
+                change_status.visibility = View.VISIBLE
+                infa3.visibility = View.INVISIBLE
+            }
+            else {
+                edit.text = "Редактировать профиль"
+                infa3.text = change_status.text
+                change_status.visibility = View.INVISIBLE
+                infa3.visibility = View.VISIBLE
+            }
+            flag = !flag
+        }
     }
 
 }
